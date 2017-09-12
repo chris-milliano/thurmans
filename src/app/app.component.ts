9f88d0dcbfb7e690be1e4111e0c4b7f98e686182
Chris Milliano <MillianoC@gmail.com>
2017-09-12 00:56:25 -0500

    FEAT: Basic layout of header and beer menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// declare const FB: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+
+    ngOnInit() {
+        console.log("Init app.component");
+
+        // FB.api( "/thurmansinshaw/events", function (response) {
+        //     if (response && !response.error) {
+        //         console.log("RESPONSE:")
+        //         console.log(response);
+        //     }
+        //     else {
+        //         console.log("nothing came");
+        //     }
+        // });
+    }
 }
